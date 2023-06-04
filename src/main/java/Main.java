4ecd1c7ad026ca4dc15b103257a906342f8d45f5
AndNata <a.n.d.n.a@yandex.ru>
2023-06-04 15:42:53 +0300

    Second commit with corrections in first project practicum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,15 @@
-import static java.lang.String.format;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-
-        try {
+        Scanner sc = new Scanner(System.in);
         Products.addProducts();
         double countMoney = Products.getSumm();
         int countFriends = CountFriends.inputCountFriends(countMoney);
         double manyForOne = CountFriends.calculate(countFriends, countMoney);
 
         System.out.println(Products.getListProducts());
-        System.out.println("Каждый должен заплатить по: "+ String.format("%.2f", manyForOne).replace(",", ".") +" "+ CountFriends.ruble(manyForOne));
-        } catch (Exception e) {
-            System.out.println("Ошибка: "+e.getMessage());
-        }
-
-
+        System.out.println("Каждый должен заплатить по: " + String.format("%.2f", manyForOne).replace(",", ".") + " " + CountFriends.ruble(manyForOne));
+        sc.close();
     }
-
 }
